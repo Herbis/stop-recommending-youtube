605e1d62703eb87c0560dfeefa76c9db98ba809f
Herberts Markuns <herbis33@gmail.com>
2024-02-02 09:54:47 +0200

    Fix scroll not being prevented on page load and page change for Shorts

--- js/content.js
+++ js/content.js
@@ -1,15 +1,31 @@
-window.onload = function() {
-    preventShortsScroll();
-}
+const observeUrlChange = () => {
+    let initialShortsScrollPrevented = false;
+    let oldHref = document.location.href;
+    const body = document.querySelector("body");
+    const observer = new MutationObserver(mutations => {
+        if (!initialShortsScrollPrevented && oldHref.includes('shorts')) {
+            initialShortsScrollPrevented = preventShortsScroll();
+        } else if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            preventShortsScroll();
+        }
+    });
+    observer.observe(body, { childList: true, subtree: true });
+  };
+
+  
+window.onload = observeUrlChange;
 
 function preventShortsScroll() {
+    console.warn('preventShortsScroll');
     var ytdShortsElement = document.getElementsByTagName('ytd-shorts');
 
-    // TODO is not loaded yet. Need to wait for it to load.
-    console.warn('ytdShortsElement: ' + ytdShortsElement.length);
     if (ytdShortsElement.length > 0) {
         ytdShortsElement[0].addEventListener('wheel', preventScroll, {passive: false});
+        return true;
     }
+
+    return false;
 }
 
 function preventScroll(e){
